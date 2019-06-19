02f70532ae44559fd3934c2d0c82f40b1ca90e73
DennisZielstra <33607177+DennisZielstra@users.noreply.github.com>
2019-06-19 22:08:01 +0200

    Rewrote code for the openinghours.
    First code was hard coded HTML. This version contains the Openings class that gets all the openinghours in object oriented PHP.

---
+++ Openings.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once 'beheer/Connections.php'; 
+
+/**
+ * Class Openings gets all the openinghours for each day.
+ */
+class Openings
+{
+    /**
+     * Gets the days, status and openinghours for Hoogezand store.
+     * @return null|string
+     */
+    public function openingsHz() 
+    {
+        $connections = new Connections();
+        $connHoogezand = $connections->connHoogezand();
+        
+        $sqlHz = "SELECT * FROM OpeningstijdenHoogezand";
+        $resultHz = $connHoogezand->query($sqlHz);
+        $array = array();
+
+        if ($resultHz->num_rows > 0) {
+            while($rowHz = $resultHz->fetch_assoc()) {
+                $array[] = 
+                    $rowHz["Dag"] . " " . 
+                    (
+                        $rowHz["Gesloten"] === "J" ?
+                        "Gesloten<br/>" : 
+                        $rowHz["TijdVan"] . "-" . $rowHz["TijdTot"] . "u<br/>"
+                    ); 
+            }
+        }
+        
+        return $array;
+    }
+}
